8f8a84e4c7bf3542b1fb4b6841787a670a08aeec
nadiafortunesky <nadiafortunesky@gmail.com>
2022-03-23 22:37:55 -0300

    clase3

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card(title, imageUrl, body) {
+    return(
+        <div className='card-c'>
+            <div className="">
+
+            </div>
+        </div>
+    )
+}
+
+export default Card;
---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Nav.css';
+
+const Nav = () => {
+    return (
+        <div>
+        <nav>
+            <h1>Mi tienda</h1>
+            <a href="#">Home</a>
+            <a href="#">Productos</a>
+            <a href="#">Ayuda</a>
+        </nav>
+        </div>
+    )
+}
+
+export default Nav
